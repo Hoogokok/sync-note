12ba5e6b5398921a4d828cf37df0e682b7a495bc
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2023-08-27 22:35:48 +0900

    #main-first-commit: 프로젝트 세팅

---
+++ src/main/java/site/syncnote/SyncNoteApplication.java
@@ -0,0 +1,13 @@
+package site.syncnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/syncnote/SyncNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package site.syncnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
